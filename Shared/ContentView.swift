4c73a260afa09e0d5cec9772382b21dac4964600
Momo Ozawa <momo.ozawa@automattic.com>
2021-07-19 09:02:55 +0100

    Add: initial files

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Momo Ozawa on 2021/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MobilePay_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilePay_AppleApp.swift
+//  Shared
+//
+//  Created by Momo Ozawa on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct MobilePay_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
