87af2705d41ff77474dc3fc8f0c9da36f5614fb6
Momo Ozawa <momo.ozawa@automattic.com>
2021-07-19 10:14:07 +0100

    Add: PaymentCoordinator class that calls MobilePayKit

---
+++ Shared/PaymentCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  PaymentCoordinator.swift
+//  MobilePay-Apple
+//
+//  Created by Momo Ozawa on 2021/07/19.
+//
+
+import Foundation
+import StoreKit
+import MobilePayKit
+
+class PaymentCoordinator: NSObject, ObservableObject {
+
+    @Published var contentList: [PurchasableContent] = []
+
+    let paymentQueueService = PaymentQueueService()
+
+    private let productIdentifiers = Set([
+        "com.mobilepay.consumable"
+    ])
+
+    override init() {
+        super.init()
+
+        paymentQueueService.fetchProducts(for: productIdentifiers) { products in
+            self.contentList = products.map { PurchasableContent(product: $0) }
+        }
+    }
+
+    func fetchProduct(for identifier: String) -> SKProduct? {
+        return paymentQueueService.fetchProduct(for: identifier)
+    }
+
+    func purchaseProduct(_ product: SKProduct) {
+        paymentQueueService.purchaseProduct(product)
+    }
+
+    func restorePurchases() {
+        paymentQueueService.restorePurchases()
+    }
+
+}
