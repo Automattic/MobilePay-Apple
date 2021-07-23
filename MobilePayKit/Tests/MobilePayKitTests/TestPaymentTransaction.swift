56d797e1165fe002fafad559fed93c74e106c1df
Momo Ozawa <momo.ozawa@automattic.com>
2021-07-23 14:49:26 +0100

    Add: TestPaymentTransaction subclass

---
+++ MobilePayKit/Tests/MobilePayKitTests/TestPaymentTransaction.swift
@@ -0,0 +1,32 @@
+import StoreKit
+
+class TestPaymentTransaction: SKPaymentTransaction {
+
+    let testPayment: SKPayment
+    let testTransactionState: SKPaymentTransactionState
+
+    init(payment: SKPayment, transactionState: SKPaymentTransactionState) {
+        testPayment = payment
+        testTransactionState = transactionState
+        super.init()
+    }
+
+    override var payment: SKPayment {
+        return testPayment
+    }
+
+    override var transactionState: SKPaymentTransactionState {
+        return testTransactionState
+    }
+}
+
+extension TestPaymentTransaction {
+    
+    static func fixture(
+        productIdentifier: String = "com.mobilepaykit.product",
+        transactionState: SKPaymentTransactionState
+    ) -> TestPaymentTransaction {
+        let testProduct = TestProduct(productIdentifier: productIdentifier)
+        return TestPaymentTransaction(payment: SKPayment(product: testProduct), transactionState: transactionState)
+    }
+}
