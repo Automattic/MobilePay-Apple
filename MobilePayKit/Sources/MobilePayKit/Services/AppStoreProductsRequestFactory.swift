0da09e660847a4034a70039040a978842c6330bd
Momo Ozawa <momo.ozawa@automattic.com>
2021-07-26 11:43:34 +0100

    Add: AppStoreProductsRequestFactory that creates a ProductsRequestHelper

---
+++ MobilePayKit/Sources/MobilePayKit/Services/AppStoreProductsRequestFactory.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol ProductsRequestFactory {
+    func createRequest(with identifiers: Set<String>, completion: @escaping FetchCompletionCallback) -> ProductsRequest
+}
+
+class AppStoreProductsRequestFactory: ProductsRequestFactory {
+    func createRequest(with identifiers: Set<String>, completion: @escaping FetchCompletionCallback) -> ProductsRequest {
+        return ProductsRequestHelper(productIdentifiers: identifiers, completion: completion)
+    }
+}
