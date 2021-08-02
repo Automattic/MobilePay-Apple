af2d289357e3780c69f0eb885235bcfb599d77ea
Momo Ozawa <momo.ozawa@automattic.com>
2021-08-02 16:59:04 +0100

    Add: InAppPurchases service

---
+++ Source/Services/InAppPurchasesService.swift
@@ -0,0 +1,36 @@
+import Combine
+import Foundation
+
+protocol InAppPurchasesServiceProtocol {
+    func fetchProductSKUs() -> AnyPublisher<[String], Error>
+    func createOrder(identifier: String, price: Int, country: String, receipt: String) -> AnyPublisher<Int, Error>
+}
+
+class InAppPurchasesService: InAppPurchasesServiceProtocol {
+
+    let networking: Networking
+
+    init(networking: Networking = URLSession.shared) {
+        self.networking = networking
+    }
+
+    func fetchProductSKUs() -> AnyPublisher<[String], Error> {
+        let productIdentifiers = [
+            "com.mobilepay.consumable.rocketfuel",
+            "com.mobilepay.consumable.premiumrocketfuel"
+        ]
+
+        return Future { $0(.success(productIdentifiers)) }
+            .delay(for: 0.5, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+
+    func createOrder(identifier: String, price: Int, country: String, receipt: String) -> AnyPublisher<Int, Error> {
+        let orderIdentifier = 1
+
+        return Future { $0(.success(orderIdentifier)) }
+            .delay(for: 0.5, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+
+}
