71a8ca4d33b2b66ff782381f74af7061d09dfdf6
Momo Ozawa <momo.ozawa@automattic.com>
2021-08-05 16:34:15 +0100

    Refactor: rename PaymentManger to MobilePayKit

--- Example/iOS/MobilePayKit-iOS/ProductListViewModel.swift
+++ Example/iOS/MobilePayKit-iOS/ProductListViewModel.swift
@@ -4,9 +4,7 @@ import MobilePayKit
 
 class ProductListViewModel: ObservableObject {
 
-    @Published private(set) var products: [MobilePayKit.Product]
-
-    private let paymentManager: PaymentManager
+    @Published private(set) var products: [Product] = []
 
     // Here we're going to store all the completed purchases
     private var completedPurchases: [String] = [] {
@@ -29,17 +27,14 @@ class ProductListViewModel: ObservableObject {
         }
     }
 
-    init(paymentManager: PaymentManager = .init()) {
-        self.products = []
-        self.paymentManager = paymentManager
-
-        paymentManager.fetchProducts(completion: { products in
+    init() {
+        MobilePayKit.fetchProducts(completion: { products in
             self.products = products.map { Product(product: $0) }
         })
     }
 
     func purchaseProduct(with identifier: String) {
-        paymentManager.purchaseProduct(with: identifier, completion: { [weak self] transaction in
+        MobilePayKit.purchaseProduct(with: identifier, completion: { [weak self] transaction in
             guard let transaction = transaction else {
                 return
             }
@@ -48,6 +43,6 @@ class ProductListViewModel: ObservableObject {
     }
 
     func restorePurchases() {
-        paymentManager.restorePurchases()
+        MobilePayKit.restorePurchases()
     }
 }
--- Source/MobilePayKit.swift
+++ Source/MobilePayKit.swift
@@ -1,3 +1,53 @@
-struct MobilePayKit {
-    var text = "Hello, World!"
+import Foundation
+import StoreKit
+
+public class MobilePayKit: NSObject {
+
+    private static let sharedInstance = MobilePayKit()
+
+    private let appStoreService = AppStoreService()
+
+    // MARK: - Init
+
+    public override init() {
+        super.init()
+    }
+
+    // MARK: - Public
+
+    public class func fetchProducts(completion: @escaping FetchCompletionCallback) {
+        sharedInstance.fetchProducts(completion: completion)
+    }
+
+    public class func purchaseProduct(with identifier: String, completion: @escaping PurchaseCompletionCallback) {
+        sharedInstance.purchaseProduct(with: identifier, completion: completion)
+    }
+
+    public class func restorePurchases() {
+        sharedInstance.restorePurchases()
+    }
+}
+
+extension MobilePayKit {
+
+    // MARK: - Private
+
+    private func fetchProducts(completion: @escaping FetchCompletionCallback) {
+        appStoreService.fetchProducts(completion: completion)
+    }
+
+    private func purchaseProduct(with identifier: String, completion: @escaping PurchaseCompletionCallback) {
+
+        // Check if the product exists in the App Store before purchasing
+        guard let product = appStoreService.fetchProduct(for: identifier) else {
+            return
+        }
+
+        appStoreService.purchaseProduct(product, completion: completion)
+
+    }
+
+    private func restorePurchases() {
+        appStoreService.restorePurchases()
+    }
 }
