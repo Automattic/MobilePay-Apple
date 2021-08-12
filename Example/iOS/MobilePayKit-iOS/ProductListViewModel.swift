ecbf98fbd38cb6743ce25f9c10561b2769fa5263
Momo Ozawa <momo.ozawa@automattic.com>
2021-08-12 12:13:15 +0100

    Fix: use trailing closure
    Use a trailing closure if a function has a single closure argument and it is the final argument

--- Example/iOS/MobilePayKit-iOS/ProductListViewModel.swift
+++ Example/iOS/MobilePayKit-iOS/ProductListViewModel.swift
@@ -33,25 +33,25 @@ class ProductListViewModel: ObservableObject {
             bundleId: Bundle.main.bundleIdentifier
         )
 
-        MobilePayKit.shared.fetchProducts(completion: { result in
+        MobilePayKit.shared.fetchProducts { result in
             switch result {
             case .success(let products):
                 self.products = products.map { Product(product: $0) }
             case .failure(let error):
                 print("Error fetching products: \(error.localizedDescription)")
             }
-        })
+        }
     }
 
     func purchaseProduct(with identifier: String) {
-        MobilePayKit.shared.purchaseProduct(with: identifier, completion: { [weak self] result in
+        MobilePayKit.shared.purchaseProduct(with: identifier) { [weak self] result in
             switch result {
             case .success(let transaction):
                 self?.completedPurchases.append(transaction.payment.productIdentifier)
             case .failure(let error):
                 print("Error purchasing product: \(error.localizedDescription)")
             }
-        })
+        }
     }
 
     func restorePurchases() {
